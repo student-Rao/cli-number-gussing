90365fb86ed149339cf7be2fbd653c314df1bd38
Rao Wakeel <raowakeel895@gmail.com>
2024-07-03 12:09:07 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const RandomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        message: "Enter Your Number: ",
+        type: "number",
+        name: "MyNumber"
+    }
+]);
+if (RandomNumber === answer.MyNumber) {
+    console.log("Congratulation! You select correct number");
+}
+else {
+    console.log("You guessed wrong number");
+}
